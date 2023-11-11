9b8b34a14f6c05f7eb8738116b258458eab94f65
chandrasekhar <chandru11235@gmail.com>
2023-11-11 12:57:38 +0530

    Update 17.56

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import HomePage from "./components/HomePage";
+import Header from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -6,17 +8,8 @@ function App() {
   return (
     <div className="flex flex-col max-w-[1000px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
-        <header className="flex item-center justify-between gap-4 p-4">
-          <h1>
-            Free<span className="text-blue-400">Scribe</span>
-          </h1>
-          <button className="flex item-center gap-2">
-            <p>New</p>
-            <i className="fa-solid fa-plus"></i>
-          </button>
-        </header>
-
-        <main className="flex-1 p-4 flex flex-col justify-center">aaaaa</main>
+        <Header />
+        <HomePage />
       </section>
       <h1 className="text-green-400"> hello </h1>
     </div>
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <main className="flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 md:gap-5 justify-center pd-20">
+      <h1 className="font-semibold text-5xl sm:text-6xl md:text-7xl">
+        Free<span className="text-blue-400 bold">Scribe</span>
+      </h1>
+      <h3 className="font-medium md:text-lg">
+        Record <span className="text-blue-400">&rarr;</span> Transcribe
+        <span className="text-blue-400">&rarr;</span> Translate
+      </h3>
+      <button className="flex item-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4">
+        <p>Record</p>
+        <i className="fa-solid fa-microphone"></i>
+      </button>
+      <p className="text-base">
+        Or{" "}
+        <label className="text-blue-400 crusor-pointer hover:text-blue-600 duration-200">
+          upload <input className="hidden" type="file" accept=".mp3,.wave" />
+        </label>
+        a mp3 file
+      </p>
+      <p className="italic text-slate-500">Free now free forever</p>
+    </main>
+  );
+}
